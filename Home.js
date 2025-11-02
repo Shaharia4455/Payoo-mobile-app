ed69cc5c8efe535226befa4a4332f387f6fc6f62
Shaharia4455 <mdshaharia4455@gmail.com>
2025-11-02 23:56:23 +0600

    toggling all card

--- Home.js
+++ Home.js
@@ -1,17 +1,58 @@
 
 const validPin = 1234
 
+// function to get value
+
+function getInputValueNum(id){
+    const inputFild = document.getElementById(id).value
+    const inputFildValue = parseInt(inputFild)
+    return inputFildValue
+}
+
+function getInputValue(id){
+    const inputFildValue = document.getElementById(id).value
+    return inputFildValue
+}
+
+// function to get innerText
+
+function getInnerText(id){
+    const element = document.getElementById(id)
+    const elementValue = element.innerText
+    const elementValueNum = parseInt(elementValue)
+    return elementValueNum
+}
+
+// function to set innerText
+
+function setInnerText(value){
+    const availableBalance = document.getElementById("available-balance")
+     availableBalance.innerText = value
+
+}
+
+// function to get cards toggling 
+function togglingCardHandle(id){
+    const forms = document.getElementsByClassName("form")
+    
+     for(const from of forms){
+        from.style.display = "none"
+     }
+
+     document.getElementById(id).style.display = "block"
+}
+
 //  add money from button
 
 document.getElementById("add-money").addEventListener("click", function(e){
     e.preventDefault()
 
-    const bank = document.getElementById("bank").value 
-    const accountNumberAdd = document.getElementById("account-number").value
-    const addAmount = parseInt(document.getElementById("add-amount").value)
-    const addPin = parseInt(document.getElementById("add-pin").value)
+    const bank = getInputValue("bank") 
+    const accountNumberAdd = getInputValue ("account-number")
+    const addAmount = getInputValueNum("add-amount")
+    const addPin = getInputValueNum("add-pin")
 
-    const availableBalance =parseInt(document.getElementById("available-balance").innerText)
+    const availableBalance = getInnerText("available-balance")
 
     if(accountNumberAdd.length < 11 ){
         alert("Please provide the valid account Number")
@@ -25,7 +66,7 @@ document.getElementById("add-money").addEventListener("click", function(e){
 
     const toatalNewAvailableBalance = addAmount + availableBalance 
 
-    document.getElementById("available-balance").innerText  = toatalNewAvailableBalance
+    setInnerText(toatalNewAvailableBalance)
 
 })
 
@@ -33,12 +74,11 @@ document.getElementById("add-money").addEventListener("click", function(e){
 document.getElementById("cash-out-btn").addEventListener("click", function(e){
     e.preventDefault()
 
-    const agentNumber = document.getElementById("agent-number").value 
-    const cashOutAmount = parseInt(document.getElementById("cash-out-amount").value)
-    const cashOutPin = parseInt(document.getElementById("cash-out-pin").value)
+    const agentNumber = getInputValue("agent-number")
+    const cashOutAmount = getInputValueNum("cash-out-amount")
+    const cashOutPin = getInputValueNum("cash-out-pin")
 
-    const availableBalance =parseInt(document.getElementById("available-balance").innerText)
-    console.log(availableBalance, agentNumber, cashOutAmount, cashOutPin)
+    const availableBalance = getInnerText("available-balance")
 
     if(agentNumber.length < 11 ){
         alert("Please provide the valid account Number")
@@ -51,27 +91,38 @@ document.getElementById("cash-out-btn").addEventListener("click", function(e){
     }
 
     const toatalNewAvailableBalance = availableBalance - cashOutAmount
-    console.log(toatalNewAvailableBalance)
 
-    document.getElementById("available-balance").innerText  = toatalNewAvailableBalance 
+    setInnerText(toatalNewAvailableBalance) 
 
 })
 
 // *** toggling feature ***
 // add money card click
-
 document.getElementById("add-money-card").addEventListener("click", function(){
 
-    document.getElementById("cash-out-from-section").style.display = "none"
-    document.getElementById("add-money-from-section").style.display = "block"
-
+    togglingCardHandle("add-money-from-section")
 })
 
 // Cash Out card click
-
 document.getElementById("cash-out-card").addEventListener("click", function(){
 
-    document.getElementById("add-money-from-section").style.display = "none"
-    document.getElementById("cash-out-from-section").style.display = "block"
+    togglingCardHandle("cash-out-from-section")
+})
 
+// Transfer money card click
+document.getElementById("transfer-money-card").addEventListener("click", function(){
+
+    togglingCardHandle("transfer-money-from-section")   
+})
+
+// Cash Out card click
+document.getElementById("get-bonus-card").addEventListener("click", function(){
+
+    togglingCardHandle("get-bonus-from-section")
+})
+
+// Cash Out card click
+document.getElementById("pay-bill-card").addEventListener("click", function(){
+
+    togglingCardHandle("pay-bill-from-section")
 })
